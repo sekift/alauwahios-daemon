df6b703cbcae5ab701530d345d0e3e78f59d92b4
Mr.lu <sekift@163.com>
2018-07-31 17:22:12 +0800

    优化
    优化

--- src/cn/alauwahios/daemon/site/Get57FXDB.java
+++ src/cn/alauwahios/daemon/site/Get57FXDB.java
@@ -16,7 +16,7 @@ import cn.alauwahios.daemon.vo.FxZiyuanVO;
 
 public class Get57FXDB implements Runnable {
 	private static final Logger logger = LoggerFactory.getLogger(Get57FXDB.class);
-	private static final String HOME_URL = "https://www.57fx.com";
+	private static final String HOME_URL = "http://www.wwaaffxx.com"; //57FX
 	private static final String VIDEO_URL = HOME_URL + "/file-sp-new-1/";// 视频资源
 
 	public void run() {
@@ -28,6 +28,8 @@ public class Get57FXDB implements Runnable {
 		Elements elesFn = doc.getElementsByClass("c_fn").select("a");
 
 		try {
+			//判断爬取到什么地方
+			Date lastPostTime = AlauwahiosDao.getFxMaxPostTime();
 			for (int i = 0; i < elesFn.size(); i++) {//
 				String fxUrl = HOME_URL + elesFn.get(i).attr("href");
 				Elements contentEles = JsoupUtil.getDocByConnect(fxUrl).getElementsByClass("content");
@@ -39,14 +41,20 @@ public class Get57FXDB implements Runnable {
 				// postTime 提交时间
 				String fxKW = fxLink.split("/")[4];
 				String postTimeString = contentEles.select("li").get(1).text();
-				String postTimeStr = postTimeString.replace("更新时间：", "") + ":00:00";
+				String postTimeStr = null;
+				if(HOME_URL.contains("57fx")){
+					postTimeStr = postTimeString.replace("更新时间：", "") + ":00:00";
+				}else if(HOME_URL.contains("wwaaffxx")){
+					postTimeStr = postTimeString.replace("分享时间：", "") + ":00:00";
+				}
 				Date postTime = DateUtil.convertStrToDate(postTimeStr, DateUtil.DEFAULT_LONG_DATE_FORMAT);
-				//判断爬取到什么地方
-				Date lastPostTime = AlauwahiosDao.getFxMaxPostTime();
+				
+				//判断省流量
 				if(lastPostTime !=null && !postTime.after(lastPostTime)){
 					break;
 				}
 				
+				fxName = changeFileName(fxName);
 				// fx资源表
 				FxZiyuanVO vo = new FxZiyuanVO();
 				vo.setFxKW(fxKW);
@@ -63,6 +71,21 @@ public class Get57FXDB implements Runnable {
 		} catch (Exception e) {
 			logger.error("[57fx抓取出错了]，1小时后会重新抓取", e);
 		}
+	}
+	
+	/**
+	 * 修改文件名
+	 * 策略：1 什么微信 公众号全部去掉
+	 * 2 最后加上自己的域名（www.pan00.com）
+	 */
+	private static final String PAN00 = "(www.pan00.com)";
+	//private static final String PAN00_HOME = "http://www.pan00.com";
+
+	public static String changeFileName(String fxName){
+		fxName = fxName.replace("微信", "");
+		fxName = fxName.replace("公众号", "");
+		fxName = fxName + PAN00;
+		return fxName;
 	}
 
 	public static void main(String[] args) {
